e741893637fc9e9cbff5b1b66672edcf7b841c62
gabocas <gabriel.castrillon@tum.de>
2022-06-21 14:33:59 +0200

    Linear regression function and figures associated

--- src/functions.py
+++ src/functions.py
@@ -9,6 +9,7 @@ from matplotlib import colorbar
 import matplotlib.image as mpimg
 from matplotlib import pyplot as plt
 
+from nilearn import input_data
 
 from brainsmash.mapgen.base import Base 
 from brainsmash.mapgen.eval import base_fit
@@ -32,7 +33,7 @@ def plot_joint(x,y,s=0,x_label='',y_label='', robust=False,xlim=None,ylim=None,r
             y_med = np.nanmedian(y[y>y.min()])
             valid_ind = ((x<(x_med+(thr_data*x_mad))) & (x>(x_med-(thr_data*x_mad))) & (y<(y_med+(thr_data*y_mad))) & (y>(y_med-(thr_data*y_mad))) & (x>x.min()) & (y>y.min())) #(x>0) & (y>0) & 
         else:
-        valid_ind = ((x>np.nanmin(x)) & (y>np.nanmin(y)))
+            valid_ind = ((x>np.nanmin(x)) & (y>np.nanmin(y)))
         if plot_log:
             g = sns.jointplot(np.log(x[valid_ind]), np.log(y[valid_ind]), kind="reg",dropna=True,color='k', space=0,joint_kws={'robust':robust},xlim=xlim,ylim=ylim,truncate=truncate)#, stat_func=r,ylim=ylim
         else:
@@ -88,8 +89,8 @@ def metric2mmp(df,sel_met,roi_id,median=True,hemi='L',calc_log=False):
     return mmp_sel_met
 
 def valid_data_index(x,y,n_mad=2):
-    x[pd.isna(x)]=x.min()#0
-    y[pd.isna(y)]=y.min()#0
+    x[pd.isna(x)]=x.min()
+    y[pd.isna(y)]=y.min()
     x = x.astype(float)
     y = y.astype(float)
     if not isinstance(n_mad, str):
@@ -183,3 +184,63 @@ def plot_surf(met,met_fn,ax='',cmap='magma',vlow=0,vhigh=100,colorbar=False,fig_
                  fontsize=10)
         cbar.outline.set_visible(False)
     cur_ax.set_title(fig_title) if ax else cur_ax.title(fig_title)
+
+def read_mask_nii(mask_fn,roi_labels=[],z_score=False,**nii_fns):
+    """
+    YOU, HELP ME!
+    """
+    masker = input_data.NiftiMasker(mask_img=mask_fn)
+    data_dict = {}
+    for prop,nii_fn in nii_fns.items():
+#        print(f'{prop}:{nii_fn}')
+        data_masked = masker.fit_transform(nii_fn)
+        if((z_score) & (prop not in roi_labels)):
+            data_masked_z = np.array([])
+            for dcv in data_masked: 
+                data_masked_z = stats.zscore(dcv)[np.newaxis,:] if data_masked_z.size==0 else np.concatenate((data_masked_z,stats.zscore(dcv)[np.newaxis,:]),axis=0)
+            data_masked_z = np.nanmedian(data_masked_z,axis=0)
+            data_dict[prop+'_z'] = data_masked_z
+        if prop not in roi_labels:
+            data_masked = np.nanmedian(data_masked,axis=0)     
+        else:
+            data_masked = data_masked.astype(int).flatten()
+        data_dict[prop] = data_masked
+    return data_dict
+        
+def multiple_joinplot(df,x,y,filtered_index_lists,np_null_dists,filter_labels,palette_regplot,color_scatterplot,xlabel='',ylabel='',xlim=None,ylim=None,s=0.1,legend_bbox_to_anchor=(-0.2,-0.5),plot_legend=True,mad_thr=None,prefix_legend_title=''):
+    ps_legend_flag = True
+    for fidx,filtered_index_list in enumerate(filtered_index_lists):
+        df_filtered = df[filtered_index_list].copy()
+        if mad_thr:
+            df_filtered = df_filtered[remove_outliers(df_filtered[x].to_numpy(),mad_thr)]        
+        rp,pp = stats.pearsonr(df_filtered.loc[df_filtered[x].notnull(),x],df_filtered.loc[df_filtered[x].notnull(),y])
+        p_label = 'p_{smash}' if len(np_null_dists)>0 else 'p'
+        pnp = nonparp(rp, np_null_dists[fidx]) if len(np_null_dists)>0 else pp
+        pnp = pnp if pnp>0 else 0.001
+        ps_legend_flag = (ps_legend_flag) & (pnp==0.001)
+        label = f'{filter_labels[fidx]}={rp:.2f}({pnp:.3f})' if((pnp>0) & (~ps_legend_flag)) else f'{filter_labels[fidx]}={rp:.2f}'
+        color_scatterplot_mod = color_scatterplot if len(color_scatterplot)>0 else palette_regplot[fidx]
+        if fidx==0:
+            g = sns.JointGrid(data=df_filtered, x=x, y=y)
+            if xlim:
+                g.ax_joint.set(xlim=xlim)
+            if ylim:
+                g.ax_joint.set(ylim=ylim)
+            g.plot_joint(sns.regplot,line_kws={'linewidth':1,'color':palette_regplot[fidx]},scatter_kws={'s':s,'color':list(color_scatterplot_mod)},truncate=False)
+            g.plot_marginals(sns.kdeplot,shade=False,linewidth=2,color=palette_regplot[fidx],label=label)
+        else:
+            if len(color_scatterplot)>0:
+                sns.regplot(df_filtered[x],df_filtered[y],color=palette_regplot[fidx],ax=g.ax_joint,scatter=False,line_kws={'linewidth':1},truncate=False)
+            else:
+                sns.regplot(df_filtered[x],df_filtered[y],color=palette_regplot[fidx],ax=g.ax_joint,scatter=True,line_kws={'linewidth':1},truncate=False,scatter_kws={'s':s,'color':list(color_scatterplot_mod)})
+            sns.kdeplot(x=df_filtered[x],shade=False,linewidth=2, ax=g.ax_marg_x,color=palette_regplot[fidx],label=label)
+            sns.kdeplot(y=df_filtered[y],shade=False,linewidth=2, ax=g.ax_marg_y,color=palette_regplot[fidx])
+    g.ax_joint.set(xlabel=xlabel,ylabel=ylabel)
+    if plot_legend:
+        legend_title = r'$Pearson\ r\ ('+p_label+'$<0.001)' if(ps_legend_flag) else r'$Pearson\ r\ ('+p_label+'$)'
+        #legend_title = legend_title if len(prefix_legend_title)>0 else prefix_legend_title
+        g.ax_marg_x.legend(loc='upper left',bbox_to_anchor=(1, 1.25))
+        legend_handles = g.ax_marg_x.get_legend_handles_labels()
+        g.ax_marg_x.get_legend().remove()
+        leg = g.ax_joint.legend(legend_handles[0],legend_handles[1],bbox_to_anchor=legend_bbox_to_anchor, loc="lower left",title=legend_title,title_fontsize=16)#,title='orig_leg[1][7]'
+        leg._legend_box.align = "left"
